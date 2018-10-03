56a6551a3668d66a672c4fa15dd2484b22d0dec0
marcinzak77 <marcin.zak77@gmail.com>
2018-10-03 16:18:51 +0200

    frog

---
+++ src/main/java/Training/FrogJmp.java
@@ -0,0 +1,11 @@
+package Training;
+
+public class FrogJmp {
+    public int solution(int X, int Y, int D) {
+        // write your code in Java SE 8
+        if ((Y - X) % D == 0) {
+            return (Y-X) / D;
+        }
+        return ((Y-X) / D) + 1;
+    }
+}
